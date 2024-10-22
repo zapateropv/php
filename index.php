3680db6479286a3311c961fd10999f98856e0947
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2024-10-22 19:16:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    echo 'hi <br>';
+    echo 'hello php';
+    //this is a comment
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>oder a pizza</button>
+</body>
+</html>
