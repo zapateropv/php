04d2924a07c39dfda9bff7d48373842150230b52
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2024-10-22 19:34:59 +0800

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,24 @@
+<?php
+
+    //STRINGS
+    $name = "Bro Code";
+    $food = "pizza";
+    $email = "bro@gmail.com";
+
+    //INT
+    $age = 21;
+    $users = 2;
+    $quantity = 3;
+
+    //FLOAT
+    $gpa = 2.5;
+    $price = 4.99;
+    $total = $quantity * $price;
+
+    //BOOLEAN
+    $employed = true;
+    $isonline = false;
+
+    echo "you have ordered {$quantity} x {$food} <br>" ;
+    echo "Your total quantity is \${$total}"
+?>
